1726be25530aff689e88b2572cdaf536a390cf00
ryanchuabowen-ops <ryanchuabowen@gmail.com>
2025-12-16 12:53:52 +0800

    Add files via upload

---
+++ board scrape.ps1
@@ -0,0 +1,20 @@
+﻿
+
+$board = Read-Host -Prompt "Give board, faggot(no slashes, just letter)"
+$4chan = Invoke-WebRequest -Uri ('boards.4chan.org/' + $board + '/')
+
+#more fluid now, more concise
+@($4chan.Links.href).ForEach({
+#for security purposes after getting compiled need a way to abort process. every time the foreach loop goes through the code, app detects if shift keys are pressed. also, removed write-host in the actual codes since we only use write-host to during development to check if app prints out correct things to make sure if app is functioning the way I wanted it to. 
+
+if($_ -Match "thread"){
+if($_ -notmatch "#"){
+$inside = Invoke-WebRequest -Uri ("boards.4chan.org/"+ $board +"/" + $_)
+@($inside.Images.src).ForEach({
+$insideinside = $_ -replace "s",""|Split-Path -Leaf
+Invoke-WebRequest -Uri ('i.4cdn.org/b/' + $insideinside) -OutFile ("C:\Users\Public\Pictures\Image Scraper\" + $insideinside)
+})
+}
+}
+
+})
---
+++ threadlink.ps1
@@ -0,0 +1,18 @@
+﻿
+$threadLink = Read-Host -Prompt "Type in thread link(entire link, nigglet)"
+$4chan = Invoke-WebRequest -Uri $threadLink
+
+#more fluid now, more concise
+@($4chan.Links.href).ForEach({
+
+if($_ -Match "i.4cdn.org" -and $_ -notmatch "#"){
+
+$temp = $_ -replace "//","https://"
+$number = $_ -replace "s",""|Split-Path -Leaf
+$inside = Invoke-WebRequest -Uri ($temp)
+
+Invoke-WebRequest -Uri $temp -OutFile ("C:\Users\Public\Pictures\Image Scraper\" + $number)
+
+}
+
+})
